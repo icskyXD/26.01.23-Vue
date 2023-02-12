a60701a2d9722eea1a359c0e339c264322e89bb7
icsky <daniyarov.alihser9200@gmail.com>
2023-02-12 13:59:20 +0600

    fc

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script>
+    export default {
+        data() {
+            return {
+                linkArray: ['https://picsum.photos/200','https://picsum.photos/201','https://picsum.photos/202','https://picsum.photos/203'],
+                interval: 2000
+            }
+        },
+        methods: {
+            addPost(){
+                this.amount++
+            }
+        }
+    }
+</script>
+ 
+<template>
+    <img v-href-change="{images: linkArray, interval: interval}" alt="">
+    <p>2</p>
+</template>
+
+<style scoped>
+</style>
